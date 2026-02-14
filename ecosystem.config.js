9978009429075bfbc2913abae0efdcc381eb8f30
Danta23 <herdanta23@gmail.com>
2026-02-14 16:56:09 +0700

    update baru sekarang

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,18 @@ module.exports = {
     {
       name: "flask",
       script: "app.py",
-      interpreter: "python"
+      interpreter: "python",   // pastikan python ada di PATH
+      env: {
+        PYTHONUNBUFFERED: "1"  // biar log langsung muncul
+      }
     },
     {
       name: "bridge",
       script: "bridge.js",
-      interpreter: "node"
+      interpreter: "node",
+      env: {
+        PYTHON_URL: "http://127.0.0.1:5000" // contoh environment variable
+      }
     }
   ]
 }
